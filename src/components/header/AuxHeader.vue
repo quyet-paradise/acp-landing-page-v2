d16f9f6a89f13509d2075ebf28cc1da45f53a520
Quyet Tran <quyet.trantien0402@gmail.com>
2026-03-02 00:37:56 +0700

    - [ACPv2] update homepage

---
+++ src/components/header/AuxHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="aux-header">
+        <div class="aux-header--item link">PATIENT LOGIN</div>
+        <div class="aux-header--item">|</div>
+        <div class="aux-header--item link">PROVIDER LOGIN</div>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.aux-header {
+    width: 100%;
+    padding: 24px 64px;
+    display: flex;
+    justify-content: flex-end;
+    background: var(--dls-color-sunlight);
+    align-items: center;
+    gap: 8px;
+
+    &--item {
+        font-family: 'Jost';
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 145%;
+        letter-spacing: -0.5%;
+        color: var(--dls-color-burgundy);
+
+        &.link {
+            cursor: pointer;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+}
+</style>
