aaaf5ae4050a099820f690991175a83e59ef4026
Quyet Tran <quyet.trantien0402@gmail.com>
2026-05-31 13:31:01 +0700

    - [ACPv2] fix feebacks

--- src/containers/home/components/HomeAccreditation.vue
+++ src/containers/home/components/HomeAccreditation.vue
@@ -2,14 +2,16 @@
     <div class="home-accreditation" data-aos="fade-up" data-aos-delay="200">
         <h2 class="home-accreditation--title">Proud Member Of</h2>
         <div class="home-accreditation--imgs">
-            <img class="apc" src="/images/accreditation/apc.png" alt="apc">
+            <img class="apc" @click="openBlankUrl('https://a4pc.org/')" src="/images/accreditation/apc.png" alt="apc">
             <!-- <img class="ldn" src="/images/accreditation/ldn.png" alt="ldn"> -->
-            <img class="pcca" src="/images/accreditation/pcca.png" alt="pcca">
+            <img class="pcca" @click="openBlankUrl('https://www.pccarx.com/')" src="/images/accreditation/pcca.png" alt="pcca">
         </div>
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { openBlankUrl } from '@/utils/common-utils'
+</script>
 <style lang="scss" scoped>
 .home-accreditation {
     width: 100%;
@@ -35,6 +37,7 @@
         .pcca, .apc, .ldn {
             height: 72px;
             width: auto;
+            cursor: pointer;
         }
     }
 
--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -3,3 +3,7 @@ import router from '@/router'
 export const goToPage = (routeName: string) => {
     router.push({name: routeName})
 }
+
+export const openBlankUrl = (url: string) => {
+    window.open(url, '_blank')
+}
